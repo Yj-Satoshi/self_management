95ebcff387dd88d40c305b86bc147d99b67c78ee
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-11-02 20:14:20 +0900

    Initial commit

---
+++ account/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    class Meta:
+        db_table = 'custom_user'
+    
+    birth_year = models.IntegerField(verbose_name='生年(西暦)', null=True, blank=True)
+    gender = models.CharField(verbose_name='性別', max_length=5, null=True, blank=True)
+    profession = models.CharField(verbose_name='職業', max_length=15, null=True, blank=True)
