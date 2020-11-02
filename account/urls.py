a8505852dc9079be4e13e03c0d0aa30046295be4
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-11-03 02:56:09 +0900

    indexページ作成

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'account'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('', views.index, name='index'),
     # path('signout/', views.signout, name='signout'),
     # path('register/', views.register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
 # from django.views.generic import View
 # Create your views here.
 
@@ -7,3 +8,10 @@ from django.contrib.auth.forms import UserCreationForm
 def signup(request):
     form = UserCreationForm()
     return render(request, 'account/signup.html', {'form': form})
+
+
+class IndexView(TemplateView):
+    template_name = 'account/index.html'
+
+
+index = IndexView.as_view()
