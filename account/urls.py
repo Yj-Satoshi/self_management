3f9c519955588d7b025ba7bb4bd6fe9cbbf86f6b
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-11-05 03:50:02 +0900

    ログインフォーム実装

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as views_auth
 
 app_name = 'account'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views_auth.LoginView.as_view(template_name='account/index.html'), name='signin'),
     # path('signout/', views.signout, name='signout'),
-    # path('register/', views.register, name='register'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
-    'monthly_goal.apps.AccountConfig',
+    'monthly_goal.apps.MonthlyGoalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
