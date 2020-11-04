aeb73f9c0886f0d54f6746b28812465db5d3998e
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-11-05 03:50:24 +0900

    モデル修正

--- monthly_goal/models.py
+++ monthly_goal/models.py
@@ -5,20 +5,20 @@ from account.models import CustomUser
 
 class MonthlyGoal(models.Model):
     class Meta:
-        db_tabele = 'monthly_goal'
+        db_table = 'monthly_goal'
 
-    custom_user = models.ForeignKey(CustomUser, verbose_name='custom_user')
+    custom_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     year = models.IntegerField(verbose_name='year')
     month = models.IntegerField(verbose_name='month')
     category = models.CharField(verbose_name='category', max_length=255, null=True, blank=True)
     goal = models.CharField(verbose_name='goal', max_length=255)
-    why_need_goal = models.TextFie(
+    why_need_goal = models.TextField(
         verbose_name='Why_need_goal',
         max_length=255, null=True, blank=True)
     sccore = models.IntegerField(verbose_name='sccore')
     revised_goal = models.CharField(
         verbose_name='revised_goal', max_length=255, null=True, blank=True)
-    why_revise = models.TextFie(
+    why_revise = models.TextField(
         verbose_name='Why_revise',
         max_length=255, null=True, blank=True)
