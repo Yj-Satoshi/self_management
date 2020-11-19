943127c15dfdc797dceeed68dacbfe818699aaee
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-11-20 04:19:58 +0900

    weekly_actionアプリ導入・setting

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'monthly_goal.apps.MonthlyGoalConfig',
+    'weekly_action.apps.WeeklyActionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ weekly_action/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeeklyActionConfig(AppConfig):
+    name = 'weekly_action'
