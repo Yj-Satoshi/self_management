d9b0454666231346c8389fd254a0c27b1f7e9684
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-11-29 07:48:33 +0900

    form作成

--- monthly_goal/forms.py
+++ monthly_goal/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from .models import MonthlyGoal
 
 
-class GoalScoreForm(forms.Form):
+class GoalScoreForm(forms.ModelForm):
     score = forms.IntegerField(
         required=False,
         label='自己評価')
+
+    after_memo = forms.TextField(
+        required=False,
+        verbose_name='後書きメモ',
+        max_length=500, null=True,
+        help_text='評価の際の反省点などの後から振り返るメモ（未設定可）', blank=True)
 
     def clean_age(self):
         score = self.cleaned_data.get('score')
@@ -16,5 +22,7 @@ class GoalScoreForm(forms.Form):
     class Meta():
         model = MonthlyGoal
         fields = (
-             'socore',
+             'socore', 'after_memo'
         )
+
+class GoalModifyForm()
