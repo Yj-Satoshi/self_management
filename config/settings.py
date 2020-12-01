0ca07812126ff2c98037a3b67b664177811c44e7
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-02 06:34:38 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SELF_MANAGEMENT_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
