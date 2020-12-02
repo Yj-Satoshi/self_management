b00dac13b5c7627fd59f35eae15a923faa68e239
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-03 00:24:12 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
