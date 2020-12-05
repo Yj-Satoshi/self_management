25f13dbf6ff396fadcd44ef733c1b0e3f9115142
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-05 19:14:11 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -15,8 +15,7 @@ import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.abspath(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # BASE_DIR = os.path.dirname(os.path.dirname(__file__))
