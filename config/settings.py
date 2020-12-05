b6df011895d613d7ae953f686d68c3467c0021ee
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-05 11:28:33 +0900

    設定変更３

--- config/settings.py
+++ config/settings.py
@@ -159,17 +159,17 @@ SASS_TEMPLATE_EXTS = ['.html', '.haml']
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-django_heroku.settings(locals())
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# django_heroku.settings(locals())
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
 
-if not DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
+# if not DEBUG:
+#     import django_heroku
+#     django_heroku.settings(locals())
 
-if DEBUG:
-    SECURE_SSL_REDIRECT = False
-else:
-    SECURE_SSL_REDIRECT = True
+# if DEBUG:
+#     SECURE_SSL_REDIRECT = False
+# else:
+#     SECURE_SSL_REDIRECT = True
