3f9dd35a62b6daa622f4ac3da3ba96db4c7929ff
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-05 15:00:05 +0900

    Update views.py

--- monthly_goal/views.py
+++ monthly_goal/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
-from django.shortcuts import redirect
 from django.views.generic import (
     DetailView,
     CreateView,
