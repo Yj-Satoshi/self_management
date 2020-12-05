aa54ad15db602a39c75fec8a0b989c4133c38438
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-05 13:08:28 +0900

    wsgi修正

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,14 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-# from whitenoise.django import DjangoWhiteNoise
-# from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-
-# application = DjangoWhiteNoise(application)
-# application = Cling(get_wsgi_application())
