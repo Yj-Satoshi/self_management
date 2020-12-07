91193ace5898c5e74cead40aec63caaf98ac17d8
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-08 06:49:02 +0900

    ユーザー単体テスト_factory_boy動作確認

--- account/tests/factory.py
+++ account/tests/factory.py
@@ -1,14 +1,19 @@
+from django.contrib.auth import get_user_model
 from factory.django import DjangoModelFactory
 from factory.fuzzy import FuzzyInteger, FuzzyText
+from factory import Faker
 from factory import Sequence
 
-from .models import CustomUser
+# from .. models import CustomUser
+
+CustomUserModel = get_user_model()
+FAKER_LOCALE = 'ja_JP'
 
 
 class CustomUserFactory(DjangoModelFactory):
 
     class Meta:
-        model = CustomUser
+        model = CustomUserModel
 
     username = Sequence(lambda n: 'user{0}'.format(n))
     password = FuzzyText(length=12, suffix='192837')
@@ -16,4 +21,4 @@ class CustomUserFactory(DjangoModelFactory):
     birth_year = FuzzyInteger(low=0, high=9999)
     gender = FuzzyInteger(low=1, high=2)
     address = FuzzyInteger(low=301, high=356)
-    profession = FuzzyText(high=15)
+    profession = Faker('text', max_nb_chars=30, locale=FAKER_LOCALE)
--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from . factory import CustomUserFactory
 # Create your tests here.
+
+
+class UserTests(TestCase):
+
+    def setUp(self):
+        self.user1 = CustomUserFactory()
+        self.user2 = CustomUserFactory(username='test_user')
+
+    def testUser(self):
+        self.assertTrue(self.user1, self.user2)
