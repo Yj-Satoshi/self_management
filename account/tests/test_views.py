576efbd848f20cd3345368696212c07240113804
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-16 07:12:21 +0900

    複合テスト（ユーザー登録画面遷移）

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
+from django.contrib.auth import get_user_model
+
 # Create your tests here.
+
+
+class TestSignInView(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test1', email='samples@sample.com', password='1234pass'
+        )
+
+    def test_get_success(self):
+        response = self.client.get('/signup/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['form'].errors)
+        self.assertTemplateUsed(response, 'account/signup.html')
