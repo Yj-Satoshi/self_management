67ef61801a391760e673a23c2df3202f14eb6abf
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-16 05:05:57 +0900

    Update test_models.py

--- weekly_action/tests/test_models.py
+++ weekly_action/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-# from django.db.utils import IntegrityError
+from django.db.utils import IntegrityError
 
 from .factory import CustomUserFactory
 from .factory import MonthlyGoalFactory
@@ -15,26 +15,18 @@ class MonthlyGoalTests(TestCase):
     def test_action(self):
         self.assertTrue(self.action)
 
-    # def test_user_fail_blank_user(self):
-    #     with self.assertRaises(IntegrityError):
-    #         MonthlyGoalFactory(custom_user=None)
+    def test_user_fail_blank_user(self):
+        with self.assertRaises(IntegrityError):
+            WeeklyActionFactory(custom_user=None)
 
-    # def test_user_fail_blank_goal(self):
-    #     with self.assertRaises(TypeError):
-    #         MonthlyGoalFactory(monthly_goal=None)
+    def test_user_fail_blank_action(self):
+        with self.assertRaises(TypeError):
+            WeeklyActionFactory(weekly_action=None)
 
-    # def test_user_fail_blank_year(self):
-    #     with self.assertRaises(IntegrityError):
-    #         MonthlyGoalFactory(year=None)
+    def test_user_fail_blank_week_no(self):
+        with self.assertRaises(IntegrityError):
+            WeeklyActionFactory(week_no=None)
 
-    # def test_user_fail_blank_month(self):
-    #     with self.assertRaises(IntegrityError):
-    #         MonthlyGoalFactory(month=None)
-
-    # def test_user_fail_wrong_month(self):
-    #     self.goal1 = MonthlyGoalFactory(month=-1).save()
-    #     self.assertFalse(self.goal1)
-
-    # def test_user_fail_wrong_score(self):
-    #     self.goal1 = MonthlyGoalFactory(score=-1)
-    #     self.assertFalse(self.goal1)
+    def test_user_fail_wrong_score(self):
+        self.action1 = WeeklyActionFactory(score=-1).save()
+        self.assertFalse(self.action1)
