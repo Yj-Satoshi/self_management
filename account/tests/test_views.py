53dc0c17f117e2dbe0d7534da9c879480055416a
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-19 19:57:31 +0900

    アカウント登録とログインテスト

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -12,8 +12,25 @@ class TestSignInView(TestCase):
             username='test1', email='samples@sample.com', password='1234pass'
         )
 
-    def test_get_success(self):
+    def test_get_signup_page_success(self):
         response = self.client.get('/signup/')
         self.assertEqual(response.status_code, 200)
         self.assertFalse(response.context['form'].errors)
         self.assertTemplateUsed(response, 'account/signup.html')
+
+    def test_get_signin_post_success(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['form'].errors)
+        self.assertTemplateUsed(response, 'account/index.html')
+
+    def test_get_signup_post_success(self):
+        self.client.post('/signup/', {
+            'username': 'testuser',
+            'email': 'sample@sample.com',
+            'password': 'testpass1234',
+            'password2': 'testpass1234',
+        })
+        self.client.login(username='testuser', password='testpass1234')
+        response = self.client.get('/main/')
+        self.assertTrue(response)
