e6c51142d63b50178723f6ff65ce5a9ab721b9f7
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-12-19 20:03:34 +0900

    月間目標投稿テスト準備

--- monthly_goal/tests/test_views.py
+++ monthly_goal/tests/test_views.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+
+class TestSignInView(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test1', email='samples@sample.com', password='1234pass'
+        )
+
+    def test_get_signup_post_success(self):
+        self.client.login(username='test1', password='1234pass')
+        response = self.client.get('/main/')
+        self.assertTrue(response)
