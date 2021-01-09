c8108b1dd1f232d42fe61ff622c2beb1079d8051
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2021-01-10 07:43:17 +0900

    Update settings.py

--- self_management/config/settings.py
+++ self_management/config/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import sys
+# import sys
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
