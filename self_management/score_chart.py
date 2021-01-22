d006b6fd5ff7b29552ee29ed8fb6804a7ff28d4a
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2021-01-22 11:39:04 +0900

    Update score_chart.py

--- self_management/score_chart.py
+++ self_management/score_chart.py
@@ -8,13 +8,6 @@ from django.http import HttpResponse
 matplotlib.use('Agg')
 date_string = datetime.datetime.now()
 
-# month1 = date_string.month
-# month2 = date_string.month - 1
-# month3 = date_string.month - 2
-# month4 = date_string.month - 3
-# month5 = date_string.month - 4
-# month6 = date_string.month - 5
-
 
 class month_score_chart:
     def month_count(n):
@@ -27,6 +20,7 @@ class month_score_chart:
             year = date_string.year - 1
             return year
 
+    user = request.user
     month1_goals_score_ave = MonthlyGoal.objects.filter(
                 custom_user_id=user.id, year=date_string.year, month=date_string.month
                 ).exclude(score__isnull=False).aggregate(Avg('score'))
@@ -51,38 +45,54 @@ class month_score_chart:
                 custom_user_id=user.id, year=year_count(5), month=month_count(5)
                 ).exclude(score__isnull=False).aggregate(Avg('score'))
 
-    y = [
-        month6_goals_score_ave, month5_goals_score_ave, month4_goals_score_ave,
-        month3_goals_score_ave, month2_goals_score_ave, month1_goals_score_ave
-        ]
+    def plt2svg():
+        buf = io.BytesIO()
+        plt.savefig(buf, format='svg', bbox_inches='tight')
+        s = buf.getvalue()
+        buf.close()
+        return s
 
-    x = [
-        month_count(5), month_count(4), month_count(3), month_count(2),
-        month_count(1), month_count(0)
-        ]
 
-    plt.plot(x, y, color='#00d5ff')
-    plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
-    plt.xlabel("月")
-    plt.ylabel("目標評価平均")
-    # def setPlt():
-    #     y = [
-    #           month6_goals_score_ave, month5_goals_score_ave, month4_goals_score_ave,
-    #           month3_goals_score_ave, month2_goals_score_ave, month1_goals_score_ave
-    #           ]
-    #     x = [
-    #         month_score_chart.month_count(5),
-    #         month_score_chart.month_count(4),
-    #         month_score_chart.month_count(3),
-    #         month_score_chart.month_count(2),
-    #         month_score_chart.month_count(1),
-    #         month_score_chart.month_count(0)
-    #         ]
-    #     plt.plot(x, y, color='#00d5ff')
-    #     plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
-    #     plt.xlabel("月")
-    #     plt.ylabel("目標評価平均")
+    def setPlt(month6_goals_score_ave, month5_goals_score_ave, month4_goals_score_ave, month3_goals_score_ave, month2_goals_score_ave, month1_goals_score_ave):
+        y = [
+            month6_goals_score_ave, month5_goals_score_ave, month4_goals_score_ave,
+            month3_goals_score_ave, month2_goals_score_ave, month1_goals_score_ave
+            ]
+        x = [
+            month_score_chart.month_count(5),
+            month_score_chart.month_count(4),
+            month_score_chart.month_count(3),
+            month_score_chart.month_count(2),
+            month_score_chart.month_count(1),
+            month_score_chart.month_count(0)
+            ]
+        plt.plot(x, y, color='#00d5ff')
+        plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
+        plt.xlabel("月")
+        plt.ylabel("目標評価平均")
 
+    def get_svg(request, month6_goals_score_ave, month5_goals_score_ave, month4_goals_score_ave, month3_goals_score_ave, month2_goals_score_ave, month1_goals_score_ave):
+        setPlt(month6_goals_score_ave, month5_goals_score_ave, month4_goals_score_ave, month3_goals_score_ave, month2_goals_score_ave, month1_goals_score_ave)
+        svg = plt2svg()
+        plt.cla()
+        response = HttpResponse(svg, content_type='image/svg+xml')
+        return response
+
+
+    # y = [
+    #     month6_goals_score_ave, month5_goals_score_ave, month4_goals_score_ave,
+    #     month3_goals_score_ave, month2_goals_score_ave, month1_goals_score_ave
+    #     ]
+
+    # x = [
+    #     month_count(5), month_count(4), month_count(3), month_count(2),
+    #     month_count(1), month_count(0)
+    #     ]
+
+    # plt.plot(x, y, color='#00d5ff')
+    # plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
+    # plt.xlabel("月")
+    # plt.ylabel("目標評価平均")
     # fig = plt.figure()
     # ax = fig.add_subplot(111, xlabel='月', ylabel='目標評価平均')
 
