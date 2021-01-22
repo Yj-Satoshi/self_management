97ca43640f3a8fc6dc6381d03dcdd28b4c077ec4
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2021-01-22 13:28:26 +0900

    Update views.py

--- self_management/account/views.py
+++ self_management/account/views.py
@@ -153,60 +153,72 @@ class MonthScoreChart():
             year = date_string.year - 1
             return year
 
+    # def setPlt(request):
+    #     user = request.user
+    #     month1_goals_score_ave = MonthlyGoal.objects.filter(
+    #             custom_user_id=user.id, year=date_string.year, month=date_string.month
+    #             ).exclude(score__isnull=False).aggregate(Avg('score'))
+
+    #     month2_goals_score_ave = MonthlyGoal.objects.filter(
+    #                 custom_user_id=user.id, year=MonthScoreChart.year_count(1),
+    #                 month=MonthScoreChart.month_count(1)
+    #                 ).exclude(score__isnull=False).aggregate(Avg('score'))
+
+    #     month3_goals_score_ave = MonthlyGoal.objects.filter(
+    #                 custom_user_id=user.id, year=MonthScoreChart.year_count(2),
+    #                 month=MonthScoreChart.month_count(2)
+    #                 ).exclude(score__isnull=False).aggregate(Avg('score'))
+
+    #     month4_goals_score_ave = MonthlyGoal.objects.filter(
+    #                 custom_user_id=user.id, year=MonthScoreChart.year_count(3),
+    #                 month=MonthScoreChart.month_count(3)
+    #                 ).exclude(score__isnull=False).aggregate(Avg('score'))
+
+    #     month5_goals_score_ave = MonthlyGoal.objects.filter(
+    #                 custom_user_id=user.id, year=MonthScoreChart.year_count(4),
+    #                 month=MonthScoreChart.month_count(4)
+    #                 ).exclude(score__isnull=False).aggregate(Avg('score'))
+
+    #     month6_goals_score_ave = MonthlyGoal.objects.filter(
+    #                 custom_user_id=user.id, year=MonthScoreChart.year_count(5),
+    #                 month=MonthScoreChart.month_count(5)
+    #                 ).exclude(score__isnull=False).aggregate(Avg('score'))
+
+        # y = [
+        #     month6_goals_score_ave,
+        #     month5_goals_score_ave,
+        #     month4_goals_score_ave,
+        #     month3_goals_score_ave,
+        #     month2_goals_score_ave,
+        #     month1_goals_score_ave
+        #     ]
+        # x = [
+        #     MonthScoreChart.month_count(5),
+        #     MonthScoreChart.month_count(4),
+        #     MonthScoreChart.month_count(3),
+        #     MonthScoreChart.month_count(2),
+        #     MonthScoreChart.month_count(1),
+        #     MonthScoreChart.month_count(0)
+        #     ]
+        # plt.plot(x, y, color='#00d5ff')
+        # plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
+        # plt.xlabel("月")
+        # plt.ylabel("目標評価平均")
+
+    def setPlt(request):
+        x = ["07/01", "07/02", "07/03", "07/04", "07/05", "07/06", "07/07"]
+        y = [3, 5, 0, 5, 6, 10, 2]
+        plt.plot(x, y, color='#00d5ff')
+        plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
+        plt.xlabel("Date")
+        plt.ylabel("km")
+
     def plt2svg():
         buf = io.BytesIO()
         plt.savefig(buf, format='svg', bbox_inches='tight')
         s = buf.getvalue()
         buf.close()
         return s
-
-    def setPlt(request):
-        user = request.user
-        month1_goals_score_ave = MonthlyGoal.objects.filter(
-                custom_user_id=user.id, year=date_string.year, month=date_string.month
-                ).exclude(score__isnull=False).aggregate(Avg('score'))
-
-        month2_goals_score_ave = MonthlyGoal.objects.filter(
-                    custom_user_id=user.id, year=MonthScoreChart.year_count(1),
-                    month=MonthScoreChart.month_count(1)
-                    ).exclude(score__isnull=False).aggregate(Avg('score'))
-
-        month3_goals_score_ave = MonthlyGoal.objects.filter(
-                    custom_user_id=user.id, year=MonthScoreChart.year_count(2),
-                    month=MonthScoreChart.month_count(2)
-                    ).exclude(score__isnull=False).aggregate(Avg('score'))
-
-        month4_goals_score_ave = MonthlyGoal.objects.filter(
-                    custom_user_id=user.id, year=MonthScoreChart.year_count(3),
-                    month=MonthScoreChart.month_count(3)
-                    ).exclude(score__isnull=False).aggregate(Avg('score'))
-
-        month5_goals_score_ave = MonthlyGoal.objects.filter(
-                    custom_user_id=user.id, year=MonthScoreChart.year_count(4),
-                    month=MonthScoreChart.month_count(4)
-                    ).exclude(score__isnull=False).aggregate(Avg('score'))
-
-        month6_goals_score_ave = MonthlyGoal.objects.filter(
-                    custom_user_id=user.id, year=MonthScoreChart.year_count(5),
-                    month=MonthScoreChart.month_count(5)
-                    ).exclude(score__isnull=False).aggregate(Avg('score'))
-
-        y = [
-            month6_goals_score_ave, month5_goals_score_ave, month4_goals_score_ave,
-            month3_goals_score_ave, month2_goals_score_ave, month1_goals_score_ave
-            ]
-        x = [
-            MonthScoreChart.month_count(5),
-            MonthScoreChart.month_count(4),
-            MonthScoreChart.month_count(3),
-            MonthScoreChart.month_count(2),
-            MonthScoreChart.month_count(1),
-            MonthScoreChart.month_count(0)
-            ]
-        plt.plot(x, y, color='#00d5ff')
-        plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
-        plt.xlabel("月")
-        plt.ylabel("目標評価平均")
 
     def get_svg(request):
         MonthScoreChart.setPlt(request)
