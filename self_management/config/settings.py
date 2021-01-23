3e8998b468422b58c01035e63af13dad82de0640
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2021-01-23 14:32:14 +0900

    Update settings.py

--- self_management/config/settings.py
+++ self_management/config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '_(*e_07l7^f3j2wxuzxh79wh2#gmjx*v=u@*d_p0bm50pye37l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["0.0.0.0", "app-server", "localhost", "127.0.0.1"]
 
@@ -90,11 +90,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'management',
-        'USER': 'root'
-        # 'USER': 'selfmanagement',
-        # 'PASSWORD': 'self1234',
-        # 'HOST': 'db',
-        # 'POST': 3306,
+        # 'USER': 'root'
+        'USER': 'selfmanagement',
+        'PASSWORD': 'self1234',
+        'HOST': 'db',
+        'POST': 3306,
     }
 }
 
